b1a97d9d2a3d2b207be187c615d13ce4524a7414
user28707916 <kelen.omnel1@gmail.com>
2017-07-13 23:51:38 +0300

    Added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-﻿
+﻿function areIntersected(RectangleA, RectangleB) {
+	if (RectangleA.left >= (RectangleB.width + RectangleB.left) ||
+		RectangleB.left >= (RectangleA.width + RectangleA.left) ||
+		RectangleA.top >= (RectangleB.top + RectangleB.height) ||
+		RectangleB.top >= (RectangleA.top + RectangleA.height)
+	)
+		return false;
+	return true;
+}
+
+function filterVisible(RectangleA, array) {
+	return array.filter(function (func) {
+		if (func.width !== 0 && func.height !== 0)
+			return areIntersected(RectangleA, func);
+		return false;
+	});
+}
